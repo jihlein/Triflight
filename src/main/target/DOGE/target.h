9c5135dc164405f92e0dcef79ad4427e9ab87612
jihlein <jihlein@hotmail.com>
2017-08-08 19:39:39 -0600

    Servo feedback for DOGE target on RSSI defined at A06.

--- src/main/target/DOGE/target.h
+++ src/main/target/DOGE/target.h
@@ -119,7 +119,7 @@
 #define ADC24_DMA_REMAP // moves ADC2 DMA from DMA2ch1 to DMA2ch3.
 #define VBAT_ADC_PIN            PA4
 #define CURRENT_METER_ADC_PIN   PA5
-#define EXTERNAL1_ADC_PIN       PA6
+#define RSSI_ADC_PIN            PA6
 
 // mpu_int definition in sensors/initialisation.c
 #define USE_EXTI
